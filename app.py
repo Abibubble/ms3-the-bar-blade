c206d395912d911ee753c25cf15bbcf758a6d25e
Abibubble <abi.yamaha.neo@gmail.com>
2021-06-25 16:03:06 +0000

    feat: Add register functionality

--- app.py
+++ app.py
@@ -27,6 +27,25 @@ def homepage():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Check if username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+
+        mongo.db.users.insert_one(register)
+
+        # Put new user into session
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful")
     return render_template("register.html")
 
 
