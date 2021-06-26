7a37646561b6fec059b6d51ca337dd847cca1aa9
Abibubble <abi.yamaha.neo@gmail.com>
2021-06-26 22:24:35 +0000

    wip: Add footer

--- static/js/script.js
+++ static/js/script.js
@@ -31,3 +31,13 @@ $(document).ready(function(){
         });
     }
 });
+
+// -------------------------------------------------------------------- Copyright
+
+function copyrightYear() {
+    var d = new Date();
+    var y = d.getFullYear();
+    document.getElementById("copyright").innerHTML = y;
+}
+
+copyrightYear();
