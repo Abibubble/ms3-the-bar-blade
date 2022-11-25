459f5bf651813574b0de33cd4624d84a9c3f57cd
Abi Harrison <71400099+Abibubble@users.noreply.github.com>
2022-11-25 14:04:07 +0000

    Set debug to False, remove prints

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-print(app.config["MONGO_DBNAME"])
-print(app.config["MONGO_URI"])
 
 mongo = PyMongo(app)
 
@@ -435,4 +433,4 @@ def not_found(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
