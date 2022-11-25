4121153c884be1ea4867f45d9fe284362feac7d4
Abi Harrison <71400099+Abibubble@users.noreply.github.com>
2022-11-25 12:24:16 +0000

    Set debug to True

--- app.py
+++ app.py
@@ -435,4 +435,4 @@ def not_found(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
